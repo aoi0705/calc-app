f725ecdaea0c984c4a273d228dd32cd7c09695b8
aoi0705 <fumika.suki0705@gmail.com>
2024-05-02 09:26:27 +0900

    a

--- public/js/calc3.js
+++ public/js/calc3.js
@@ -108,77 +108,6 @@ $(function(){
         
         var houseclass = sessionStorage.getItem('house-class')
 
-        if(sessionStorage.getItem('house-class') == "myhouse"){
-			// 次の項目を表示
-			let box = document.querySelector('#yourhome');
-			//styleのdisplayを変更する関数
-			box.style.display='';
-        }
-        else{
-            // 次の項目を表示
-			let box = document.querySelector('#yourestate');
-			//styleのdisplayを変更する関数
-			box.style.display='';
-        }
-    });
-});
-
-$(function(){
-    window.addEventListener('load', function(){
-        if(sessionStorage.getItem('yourpartner') == "いる"){
-            var partner_bool2 = true;
-        }
-        else if(sessionStorage.getItem('yourpartner') == "いない"){
-            var partner_bool2 = false;
-        }
-        
-        if(sessionStorage.getItem('children') == "いる"){
-            var children_bool2 = true;
-        }
-        else if(sessionStorage.getItem('children') == "いない"){
-            var children_bool2 = false;
-        }
-        
-        if(sessionStorage.getItem('number-children') == "one"){
-            var children_n = 1;
-        }
-        else if(sessionStorage.getItem('number-children') == "two"){
-            var children_n = 2;
-        }
-        else if(sessionStorage.getItem('number-children') == "three"){
-            var children_n = 3;
-        }
-        else if(sessionStorage.getItem('number-children') == "four"){
-            var children_n = 4;
-        }
-        else if(sessionStorage.getItem('number-children') == "five"){
-            var children_n = 5;
-        }
-        else if(sessionStorage.getItem('number-children') == "six"){
-            var children_n = 6;
-        }
-        else if(sessionStorage.getItem('number-children') == "seven"){
-            var children_n = 7;
-        }
-        else if(sessionStorage.getItem('number-children') == "eight"){
-            var children_n = 8;
-        }
-        else if(sessionStorage.getItem('number-children') == "nine"){
-            var children_n = 9;
-        }
-        else if(sessionStorage.getItem('number-children') == "ten"){
-            var children_n = 10;
-        }
-        
-        if(sessionStorage.getItem('house-class') == "rental"){
-            var myhome = true;
-        }
-        else{
-            var myhome = false;
-        }
-        
-        var houseclass = sessionStorage.getItem('house-class')
-
         if(partner_bool2 == true && myhome == true && children_bool2 == true){
             const stat = document.querySelector('.last-state-page');
             var s = children_n * 4 + 14
@@ -218,6 +147,19 @@ $(function(){
             const stat = document.querySelector('.last-state-page');
             var s = children_n * 4 + 8
             stat.textContent = String(s)
+        }
+
+        if(sessionStorage.getItem('house-class') == "myhouse"){
+			// 次の項目を表示
+			let box = document.querySelector('#yourhome');
+			//styleのdisplayを変更する関数
+			box.style.display='';
+        }
+        else{
+            // 次の項目を表示
+			let box = document.querySelector('#yourestate');
+			//styleのdisplayを変更する関数
+			box.style.display='';
         }
     });
 });
